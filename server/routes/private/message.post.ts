05d398ad47fdea541346b3eed7a590d45d59d7b0
Novitskyi Bohdan <102917281+biguard1@users.noreply.github.com>
2025-07-24 20:06:58 +0300

    Updated messages inline keyboard and added inline keyboard in private endpoint (#14)
    * fix: updated messages with inline keyboards
    
    * feat: added inline keyboard to private endpoint
    
    * refactor: better inline keyboard retrieving
    
    * fix: removed log

--- server/routes/private/message.post.ts
+++ server/routes/private/message.post.ts
@@ -1,6 +1,16 @@
+import { InlineKeyboard } from "grammy";
+
 const requestBodySchema = z.object({
   content: z.string().min(1, "Message content cannot be empty"),
   receiverId: z.number(),
+  inlineKeyboard: z
+    .array(
+      z.object({
+        text: z.string().min(1, "Button text cannot be empty"),
+        url: z.string().url("Invalid URL format"),
+      }),
+    )
+    .optional(),
 });
 
 export default eventHandler(async (event) => {
@@ -8,9 +18,13 @@ export default eventHandler(async (event) => {
 
   try {
     const telegram = useTelegram();
-    const { content, receiverId } = validated;
+    const { content, receiverId, inlineKeyboard } = validated;
     await telegram.sendMessage(receiverId, content, {
       parse_mode: "MarkdownV2",
+      reply_markup: inlineKeyboard?.reduce(
+        (acc, { text, url }) => acc.url(text, url).row(),
+        new InlineKeyboard()
+      ),
     });
   } catch (error) {
     console.error("Error sending message:", error.message);
