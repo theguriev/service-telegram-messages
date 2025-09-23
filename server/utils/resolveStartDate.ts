200cc7ded5291263f97f4da7ce096e0562daa4f3
biguard1 <bogdasha05@gmail.com>
2025-09-23 18:08:26 +0300

    fix: fixed start date resolving

--- server/utils/resolveStartDate.ts
+++ server/utils/resolveStartDate.ts
@@ -2,11 +2,18 @@ import { startOfDay } from "date-fns";
 import { toZonedTime } from "date-fns-tz";
 import { dateDifference } from "../../constants";
 
-const resolveStartDate = (date: Date, timezone: string = "Europe/Kyiv", withoutDateDifference: boolean = false) => {
-  const startDate = startOfDay(date);
-  const zonedDate = toZonedTime(date, timezone);
+const resolveStartDate = (
+  date: Date,
+  timezone: string = "Europe/Kyiv",
+  withoutDateDifference: boolean = false,
+) => {
+  const zonedDate = new Date(
+    toZonedTime(date, timezone).getTime() -
+      (withoutDateDifference ? 0 : dateDifference.valueOf()),
+  );
+  const startDate = startOfDay(zonedDate);
   const difference = date.getTime() - zonedDate.getTime();
-  return new Date(startDate.getTime() + (withoutDateDifference ? 0 : dateDifference.valueOf()) + difference);
+  return new Date(startDate.getTime() + difference);
 };
 
 export default resolveStartDate;
