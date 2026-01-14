1db1bb4cb1137d40d956d6b27610767562801de0
Eugen Guriev <gurievcreative@gmail.com>
2026-01-15 00:36:30 +0200

    fix: update MarkdownV2 formatting in report content for consistency

--- server/utils/getReportContent.ts
+++ server/utils/getReportContent.ts
@@ -125,22 +125,22 @@ const getReportContent = async (
           ? `${new Big(grams).mul(value).round()} шт.`
           : `${new Big(grams).mul(value).round()}г`;
 
-      return md`>• *${escapeMarkdownV2(
-        name
-      )}* \\\\(${resultValue}\\\\) \\\\(${new Big(value)
+      return md`>• *${escapeMarkdownV2(name)}* (${resultValue}) (${new Big(
+        value
+      )
         .mul(100)
-        .round()}\\\\% від рекомендованої\\\\)${
+        .round()}% від рекомендованої)${
         additionalInfo?.trim()
-          ? ` \\\\- "${escapeMarkdownV2(additionalInfo.trim())}"`
+          ? ` - "${escapeMarkdownV2(additionalInfo.trim())}"`
           : ""
       }`;
     }
 
-    return md`>• *${escapeMarkdownV2(name)}* \\\\(${grams}г\\\\): ${new Big(
-      value
-    ).mul(100)}%${
+    return md`>• *${escapeMarkdownV2(name)}* (${grams}г): ${new Big(value).mul(
+      100
+    )}%${
       additionalInfo?.trim()
-        ? ` \\\\- "${escapeMarkdownV2(additionalInfo.trim())}"`
+        ? ` - "${escapeMarkdownV2(additionalInfo.trim())}"`
         : ""
     }`;
   };
@@ -249,7 +249,7 @@ const getReportContent = async (
 
   const weeklyWorkoutsText =
     md`
-> _*Тренування за тиждень \\\\(понеділок — неділя\\\\):*_
+> _*Тренування за тиждень (понеділок — неділя):*_
     ` +
     "\n" +
     md`>*Проведено:* ${user.weeklyWorkoutsCount ?? 0}`;
